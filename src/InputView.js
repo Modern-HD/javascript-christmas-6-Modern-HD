7cbb869b070fd790c5a911e87dedab8035580fa4
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2023-11-14 00:08:04 +0900

    feat: 주문을 입력 받는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,5 +1,8 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 import Menu from './Menu.js';
+import ChristmasPromotion from './ChristmasPromotion.js';
+
+const MENU_EXP = /^([a-zA-Z가-힣]+-\d+)(,[a-zA-Z가-힣]+-\d+)*$/;
 
 const InputView = {
     /**
@@ -15,6 +18,25 @@ const InputView = {
                 );
                 this.visitDateInputValidate(input);
                 return new Date(`2023-12-${input < 10 ? `0${input}` : input}`);
+            } catch (error) {
+                MissionUtils.Console.print(error.message);
+            }
+        }
+    },
+
+    /**
+     * @returns {Promise<string[]>}
+     */
+    async readMenu() {
+        while (true) {
+            try {
+                const input = await MissionUtils.Console.readLineAsync(
+                    '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n'
+                ).then(str => str.trim());
+                this.orderInputValidate(input);
+                const menus = this.getMenuStrArr(input);
+                ChristmasPromotion.orderCommonValidate(menus);
+                return menus;
             } catch (error) {
                 MissionUtils.Console.print(error.message);
             }
@@ -47,6 +69,30 @@ const InputView = {
         if (new Set(splitted.map(([menu]) => menu)).size !== splitted.length) {
             throw new Error('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
         }
+    },
+
+    /**
+     * @param {string} input
+     * @returns {[string, number][]}
+     */
+    menuInputSplit(input) {
+        return input
+            .split(',')
+            .map(str => str.split('-'))
+            .map(([menu, amount]) => [menu, Number(amount)]);
+    },
+
+    /**
+     * @param {string} input
+     * @returns {string[]}
+     */
+    getMenuStrArr(input) {
+        const MenuStrArr = [];
+        this.menuInputSplit(input).forEach(([menu, amount]) => {
+            const strArr = [...Array(amount)].fill(menu);
+            MenuStrArr.push(...strArr);
+        });
+        return MenuStrArr;
     },
 };
 
