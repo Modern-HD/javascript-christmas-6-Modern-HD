a5ab8166164ce957bc8c2443ed7641d859b41ede
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2023-11-14 00:11:12 +0900

    feat: 주문 메뉴를 출력하는 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -4,6 +4,19 @@ const OutputView = {
     printWelcomeMsg() {
         MissionUtils.Console.print('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.');
     },
+
+    /**
+     * @param {string[]} menus
+     */
+    printMenu(menus) {
+        MissionUtils.Console.print('<주문 메뉴>');
+        menus.reduce((acc, cur, i, arr) => {
+            if (cur === arr[i + 1]) return acc + 1;
+            MissionUtils.Console.print(`${cur} ${acc}개`);
+            return 1;
+        }, 1);
+        MissionUtils.Console.print('');
+    },
 };
 
 export default OutputView;
