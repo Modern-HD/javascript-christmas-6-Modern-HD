362860c98139b03e9c5fec9aa476dc9485686a33
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2023-11-14 00:16:06 +0900

    feat: 혜택 내역을 출력하는 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,4 +1,5 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
+import Menu from './Menu.js';
 
 const OutputView = {
     printWelcomeMsg() {
@@ -33,6 +34,24 @@ const OutputView = {
     printGift(isGift) {
         MissionUtils.Console.print('<증정 메뉴>');
         MissionUtils.Console.print(isGift ? '샴페인 1개' : '없음');
+        MissionUtils.Console.print('');
+    },
+
+    /**
+     * @param {[number, number, number, number, boolean]}
+     */
+    printBenefits([christmasDDaySale, weekdaySale, weekendSale, specialDaySale, isGift]) {
+        MissionUtils.Console.print('<혜택 내역>');
+        if (christmasDDaySale) {
+            MissionUtils.Console.print(`크리스마스 디데이 할인: -${this.currencyFormat(christmasDDaySale)}원`);
+        }
+        if (weekdaySale) MissionUtils.Console.print(`평일 할인: -${this.currencyFormat(weekdaySale)}원`);
+        if (weekendSale) MissionUtils.Console.print(`주말 할인: -${this.currencyFormat(weekendSale)}원`);
+        if (specialDaySale) MissionUtils.Console.print(`특별 할인: -${this.currencyFormat(specialDaySale)}원`);
+        if (isGift) MissionUtils.Console.print(`증정 이벤트: -${this.currencyFormat(Menu.get('샴페인').price)}원`);
+        if (!(christmasDDaySale || weekdaySale || weekendSale || specialDaySale || isGift)) {
+            MissionUtils.Console.print('없음');
+        }
         MissionUtils.Console.print('');
     },
 };
