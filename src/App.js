6f55fcc92b0e6762ae5cd6b99ec54b793c1ee9b5
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2023-11-14 00:19:01 +0900

    feat: run 메소드 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Calculator from './Calculator.js';
+import ChristmasPromotion from './ChristmasPromotion.js';
+import InputView from './InputView.js';
+import OutputView from './OutputView.js';
+
 class App {
-  async run() {}
+    async run() {
+        OutputView.printWelcomeMsg();
+        const visitDate = await InputView.readDate();
+        const menus = await InputView.readMenu();
+        const totalOriginPrice = Calculator.getTotalOriginPrice(menus);
+        const sales = ChristmasPromotion.getSales(visitDate, menus, totalOriginPrice);
+        const totalBenefitPrice = Calculator.getTotalBenefitPrice(sales);
+        OutputView.printResult(visitDate, menus, totalOriginPrice, totalBenefitPrice, sales);
+    }
 }
 
 export default App;
