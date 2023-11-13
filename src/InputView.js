5984bad4449928ee09ca3449dfa89b7fa2c9eca7
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2023-11-14 00:03:28 +0900

    feat: 방문할 날짜를 유효성 검사 하는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,18 @@
-export default InputView = {
+import { MissionUtils } from '@woowacourse/mission-utils';
+
+const InputView = {
     async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
-    }
-    // ...
-}
+        const input = await Console.readLineAsync('12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)');
+    },
+
+    /**
+     * @param {number} input
+     */
+    visitDateInputValidate(input) {
+        if (!(input && input >= 1 && input <= 31)) {
+            throw new Error('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+        }
+    },
+};
+
+export default InputView;
