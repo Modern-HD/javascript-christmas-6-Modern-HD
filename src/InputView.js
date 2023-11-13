7af319e45868dc3a16f6eac9536d40519d1081ff
Modern-HD <102591908+Modern-HD@users.noreply.github.com>
2023-11-14 00:04:12 +0900

    feat: 방문할 날짜를 입력받는 기능 추가

--- src/InputView.js
+++ src/InputView.js
@@ -1,8 +1,23 @@
 import { MissionUtils } from '@woowacourse/mission-utils';
 
 const InputView = {
+    /**
+     * @returns {Promise<Date>}
+     */
     async readDate() {
-        const input = await Console.readLineAsync('12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)');
+        while (true) {
+            try {
+                const input = Number(
+                    await MissionUtils.Console.readLineAsync(
+                        '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n'
+                    )
+                );
+                this.visitDateInputValidate(input);
+                return new Date(`2023-12-${input < 10 ? `0${input}` : input}`);
+            } catch (error) {
+                MissionUtils.Console.print(error.message);
+            }
+        }
     },
 
     /**
